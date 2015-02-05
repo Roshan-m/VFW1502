ce0c13e9cc541b0a717cc775f7f095972c8bfbd4
Roshan-m <rjmykoo@fullsail.edu>
2015-02-05 13:13:29 -0500

    Created Project 1 files

--- Lecture Activities/Day2LectureWi/Resources/app.js
+++ Lecture Activities/Day2LectureWi/Resources/app.js
@@ -1,5 +1,35 @@
-var win = Titanium.UI.createWindow({
-	backgroundColor:"#cccccc"
+var win= Titanium.UI.createWindow({
+	backgroundColor:"#3E7FBB",
 });
 
+var title= Titanium.UI.createLabel({
+	 text: "Goku space",
+	 top:10,
+});
+
+var view= Titanium.UI.createView({
+	backgroundImage:"goku.png",
+	width:200,
+	height:200,
+	top:50,
+});
+var view2= Titanium.UI.createView({
+	backgroundImage:"galaxy.jpg",
+	width:200,
+	height:200,
+	bottom:100
+});
+
+var view3= Titanium.UI.createView({ 
+	backgroundImage:"creeper.jpg",
+	width:200,
+	height:200,
+	bottom:10,
+});
+
+
+win.add(view);
+win.add(title);
+win.add(view2);
+win.add(view3);
 win.open();

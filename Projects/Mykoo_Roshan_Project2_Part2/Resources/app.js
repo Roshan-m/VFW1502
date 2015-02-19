bcb06497560ee727a59d0ccef6da730f916fb79b
Roshan-m <rjmykoo@fullsail.edu>
2015-02-19 10:34:50 -0500

    Code Refractoring project

---
+++ Projects/Mykoo_Roshan_Project2_Part2/Resources/app.js
@@ -0,0 +1,24 @@
+var win1 =Ti.UI.createWindow({
+	backgroundColor:"#BEE9DB",
+});
+
+var nav = Titanium.UI.iOS.createNavigationWindow({
+   window: win1
+});
+
+var win2= Ti.UI.createWindow({
+	title:'Best Games of 2014 by Console',
+});
+var win3= Ti.UI.createWindow({
+	title:'Best Games of 2014',
+});
+var loadFile= require('second');
+
+
+
+
+
+win1.open();
+win1.add(loadFile.tableView);
+win1.add(loadFile.listView);
+win1.add(loadFile.title);

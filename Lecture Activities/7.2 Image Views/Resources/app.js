06abd321b0d701580a84d09ffa77f598cf370bea
Roshan-m <rjmykoo@fullsail.edu>
2015-02-19 14:41:20 -0500

    Lecture Activity and Project 3

--- Lecture Activities/7.2 Image Views/Resources/app.js
+++ Lecture Activities/7.2 Image Views/Resources/app.js
@@ -1,64 +1,39 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+//Window
+var win = Ti.UI.createWindow({
+	backgroundColor: '#cccccc',
+	layout: "horizontal", 
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var gallery = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory,"images");
+
+var galleryList = gallery.getDirectoryListing();
+ 
+var viewContainter = Ti.UI.createScrollView({
+	layout: "horizontal",
+
 });
 
-win1.add(label1);
+// //Loop
+// var images = [
+	// 'pattern-galaxy.jpg',
+	// 'pattern-goku.jpg',
+	// 'pattern-medow.jpg',
+	// 'pattern-sponge.png',
+	// 'pattern-skull.jpg',
+// ];
+for(var i=0; i<galleryList.length; i++){
+	//ImageView
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
+	var skull = Ti.UI.createImageView({
+		image : "images/"+galleryList[i],
+		width:200,
+	});
+		//Add
+		win.add(skull);
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
+};
 
 
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+//Open Window
+win.add(viewContainter);
+win.open();
---
+++ Projects/Project3_Mykoo_Roshan/Resources/app.js
@@ -0,0 +1,5 @@
+var win1= Ti.UI.createWindow({
+	title: 'My Favorite Album Art'
+});
+
+win1.open();

e87df891593e6d7aaa46998722899596cf84948c
Roshan-m <rjmykoo@fullsail.edu>
2015-02-28 12:47:14 -0500

    Project 4 Working application

--- Projects/Mykoo_Roshan_Project4/Resources/window2.js
+++ Projects/Mykoo_Roshan_Project4/Resources/window2.js
@@ -25,35 +25,45 @@ var changeGame = Ti.UI.createLabel({
 });
 buttonView.add(changeGame);
 
-// Create a Button.
-var button = Ti.UI.createButton({
-	height : 40,
-	width : 200,
-	bottom: 100,
-});
+
 
 // Listen for click events.
-button.addEventListener('click', function() {
-	alert('button was clicked!');
-});
 
-var gallery = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'images');
+var gamesArray =[
+'images/bayonetta.jpg',
+'images/childoflight.jpg',
+'images/darksouls2.jpg',
+'images/destiny.jpg',
+'images/divinity.jpg',
+'images/dragonageinquisition.jpg',
+'images/gta5.jpg',
+'images/hearthstone.jpg',
+'images/lastofus.jpg',
+'images/mariokart.jpg',
+'images/pt.jpg',
+'images/shadowofmordor.jpg',
+'images/shovelknight.jpg',
+'images/smash4.jpg',
+];
 
-var  galleryList= gallery.getDirectoryListing();
-
-for(var i=0; i<galleryList.length; i++){
-var imageSquare = Ti.UI.createImageView({ 
-			image: 'images/' + galleryList[i],
+var imageSquare = Ti.UI.createView({ 
 			height: 314,
 			width: 220,
 			borderColor :'white',
-			id:i,
 			top: 10,
-	});
+});
+
+var randomFunctionGen = function(maximum, minimum){
+	return Math.floor(Math.random()*(maximum-minimum+1)+minimum)
 };
 
+buttonView.addEventListener('click', function(){
+	imageSquare.setBackgroundImage(gamesArray[randomFunctionGen(gamesArray.length, 1)-1]);
+	console.log('hi');
+});
+
 // Add to the parent view.
-buttonView.add(button);
+// buttonView.add(button);
 
 win2.add(imageSquare);
 win2.add(buttonView);
